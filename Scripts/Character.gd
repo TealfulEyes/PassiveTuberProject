d4b95b905e8995888beaaeccfdf580bbdb3b3925
Tealful Eyes <91493131+TealfulEyes@users.noreply.github.com>
2021-10-01 10:44:24 -0400

    Update to character.gd
    Updated comments in gdscript for character controls

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -2,24 +2,25 @@ extends Node2D
 
 var micDetection
 export var canBlink = true
+## ADD, CHANGE, OR REMOVE NUMBERS FROM THE BACKET HERE FOR RANDOMIZED BLINK TIMES ##
 var blinktime = [2,1,5,10,4]
 
 func _ready():
-	print("YOUR AUDIO CAPTURE DEVICE IS ", AudioServer.capture_get_device()) # This will print what the capture device is that's recording
+	## REMOVE "#" FROM THE BELOW LINE TO PRINT THE AUDIO CAPTURE DEVICE. IT SHOULD READ AS DEFAULT ##
+	# print("YOUR AUDIO CAPTURE DEVICE IS ", AudioServer.capture_get_device()) # This will print what the capture device is that's recording
 	randomize()
 	
 func _process(_delta):
 	micDetection = abs(AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Mic"),0))
-	if micDetection < 30: #Muted volume is 200(-200 without abs) and will decrease depending on mic. Adjust number values to best fit your setup
+	# SET THE NUMBERS BELOW TO BE THE SAME. REMOVE "#" FRIN LINE 21 TO PRINT IN REALTIME WHAT YOUR MIC IS DETECTING, IF IT SHOWS AS 200, THERE IS NO SOUND BEING DETECTED
+	if micDetection < 30: 
 		$character.play("talk")
 	elif micDetection > 30:
 		$character.play("idle")
-	# FOR OUTPUT TO MAKE SURE CHANGES ARE HAPPENING AS THEY SHOULD
-	## MICROPHONE VOLUME (THIS CAN HELP WITH SENSITIVITY WHEN TALKING SHOULD AND SHOULDNT TRIGGER) ##
+	## PRINTS THE REAL TIME MICROPHONE VOLUME (THIS CAN HELP WITH SENSITIVITY WHEN TALKING SHOULD AND SHOULDNT TRIGGER) ##
 	# print("Current Volume is ", micDetection)
 	## BLINK TIMER - REMOVE "#" FROM LINE BELOW TO SEE WHAT THE TIME IS SET TO ##
 	# print($Timer.wait_time)
-
 
 func _on_Timer_timeout():
 	$eyes.play("blinking")
