5e8101425664c116432fdcc25b3f61219a1bc5e2
Tealful Eyes <91493131+TealfulEyes@users.noreply.github.com>
2021-09-30 22:35:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PassiveGodotVtuber
+ A Passive Vtuber Godot project template for streamers to setup simple characters with.
---
+++ Scripts/Character.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var micDetection
+export var canBlink = true
+var blinktime = [2,1,5,10,4]
+
+func _ready():
+	print("YOUR AUDIO CAPTURE DEVICE IS ", AudioServer.capture_get_device()) # This will print what the capture device is that's recording
+	randomize()
+	
+func _process(_delta):
+	micDetection = abs(AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Mic"),0))
+	if micDetection < 30: #Muted volume is 200(-200 without abs) and will decrease depending on mic. Adjust number values to best fit your setup
+		$character.play("talk")
+	elif micDetection > 30:
+		$character.play("idle")
+	# FOR OUTPUT TO MAKE SURE CHANGES ARE HAPPENING AS THEY SHOULD
+	## MICROPHONE VOLUME (THIS CAN HELP WITH SENSITIVITY WHEN TALKING SHOULD AND SHOULDNT TRIGGER) ##
+	# print("Current Volume is ", micDetection)
+	## BLINK TIMER ##
+	# print($Timer.wait_time)
+
+
+func _on_Timer_timeout():
+	$eyes.play("blinking")
+
+func _on_eyes_animation_finished():
+	$eyes.stop()
+	$Timer.wait_time = blinktime[randi() % blinktime.size()]
+	$Timer.start()
