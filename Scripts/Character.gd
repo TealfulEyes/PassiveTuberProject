2611a56b9bec39b7022b4fd09c3f00ab5a91f0f5
Tealful Eyes <contact@tealfuleyes.com>
2024-02-21 14:44:29 -0500

    Fixed blinking issue and starting on work to make Mic detection an exported threshold value

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var micDetection
 @export var canBlink = true
+@export var micThreshold : float
 ## ADD, CHANGE, OR REMOVE NUMBERS FROM THE BACKET HERE FOR RANDOMIZED BLINK TIMES ##
 var blinktime = [2,1,5,10,4]
 
@@ -13,19 +14,18 @@ func _ready():
 func _process(_delta):
 	micDetection = abs(AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Mic"),0))
 	# SET THE NUMBERS BELOW TO BE THE SAME. REMOVE "#" FRIN LINE 21 TO PRINT IN REALTIME WHAT YOUR MIC IS DETECTING, IF IT SHOWS AS 200, THERE IS NO SOUND BEING DETECTED
-	if micDetection < 30: 
+	if micDetection < micThreshold: 
 		$character.play("talk")
-	elif micDetection > 30:
+	elif micDetection > micThreshold:
 		$character.play("idle")
 	## PRINTS THE REAL TIME MICROPHONE VOLUME (THIS CAN HELP WITH SENSITIVITY WHEN TALKING SHOULD AND SHOULDNT TRIGGER) ##
-	# print("Current Volume is ", micDetection)
+	print("Current Volume is ", micDetection)
 	## BLINK TIMER - REMOVE "#" FROM LINE BELOW TO SEE WHAT THE TIME IS SET TO ##
-	# print($Timer.wait_time)
-
-func _on_Timer_timeout():
-	$eyes.play("blinking")
+	#print($Timer.wait_time)
 
 func _on_eyes_animation_finished():
-	$eyes.stop()
-	$Timer.wait_time = blinktime[randi() % blinktime.size()]
-	$Timer.start()
+	$Timer.set_wait_time(blinktime.pick_random())
+	#print($Timer.wait_time)
+	
+func _on_timer_timeout():
+	$eyes.play("blinking")
